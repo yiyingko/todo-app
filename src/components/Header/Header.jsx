be847fbd543fefc284a56f3b9e35f33ea60e359d
YiYing Ko <ykocuriosity@gmail.com>
2026-02-07 20:21:21 +0000

    feat:background img on header, icon, test @media on desktop width

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
-import iconMoon from '../../assets/images/icon-moon.svg';
+import iconSun from '../../assets/images/icon-sun.svg';
+import './Header.css';
 
 export default function Header() {
   return (
     <>
-      <header>
-        <div className="header__wrapper">
-          <h1>To Do</h1>
-          <img src={iconMoon} alt="dark-mode-icon" />
+      <header className="hero">
+        <div className="hero__wrapper">
+          <h1>TODO</h1>
+          <img src={iconSun} alt="sun-icon" className="hero__icon" />
         </div>
       </header>
     </>
